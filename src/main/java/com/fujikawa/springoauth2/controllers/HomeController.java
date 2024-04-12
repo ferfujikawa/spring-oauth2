979733adfabf4067c66d981da0fd18019c13a5a2
Fernando Fujikawa <ferfujikawa@gmail.com>
2024-04-12 11:30:14 -0300

    Criando controller home e página index.html.

---
+++ src/main/java/com/fujikawa/springoauth2/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.fujikawa.springoauth2.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+}
